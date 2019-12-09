67996d79de1e281169cbcf05623aa48fcbc9f50e
olga3n <olga3n@gmail.com>
2019-12-09 21:02:49 +0300

    09

---
+++ 2019/09-sensor-boost-2.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def resolve_arg(instruction, relative_base, index, registers, ip):
+
+    ax_mode = int(instruction[-3 - index])
+
+    if ax_mode == 0:
+        ax = registers[ip + 1 + index]
+        ax_type = 'position'
+    elif ax_mode == 1:
+        ax = registers[ip + 1 + index]
+        ax_type = 'value'
+    elif ax_mode == 2:
+        ax = relative_base + registers[ip + 1 + index]
+        ax_type = 'position'
+
+    if ax_type == 'position':
+        while len(registers) <= ax:
+            registers.append(0)
+
+    return (ax_type, ax)
+
+
+def operation_add(ip, instruction, relative_base, registers):
+    ax_type, ax = resolve_arg(instruction, relative_base, 0, registers, ip)
+    bx_type, bx = resolve_arg(instruction, relative_base, 1, registers, ip)
+    cx_type, cx = resolve_arg(instruction, relative_base, 2, registers, ip)
+
+    arg1 = registers[ax] if ax_type == 'position' else ax
+    arg2 = registers[bx] if bx_type == 'position' else bx
+
+    registers[cx] = arg1 + arg2
+
+    return ip + 4
+
+
+def operation_mul(ip, instruction, relative_base, registers):
+    ax_type, ax = resolve_arg(instruction, relative_base, 0, registers, ip)
+    bx_type, bx = resolve_arg(instruction, relative_base, 1, registers, ip)
+    cx_type, cx = resolve_arg(instruction, relative_base, 2, registers, ip)
+
+    arg1 = registers[ax] if ax_type == 'position' else ax
+    arg2 = registers[bx] if bx_type == 'position' else bx
+
+    registers[cx] = arg1 * arg2
+
+    return ip + 4
+
+
+def operation_get_input(
+        ip, instruction, relative_base, registers, input_value):
+
+    ax_type, ax = resolve_arg(instruction, relative_base, 0, registers, ip)
+
+    registers[ax] = input_value
+
+    return ip + 2
+
+
+def operation_set_output(ip, instruction, relative_base, registers):
+    ax_type, ax = resolve_arg(instruction, relative_base, 0, registers, ip)
+
+    arg1 = registers[ax] if ax_type == 'position' else ax
+
+    return (ip + 2, arg1)
+
+
+def operation_jump_if_true(ip, instruction, relative_base, registers):
+    ax_type, ax = resolve_arg(instruction, relative_base, 0, registers, ip)
+    bx_type, bx = resolve_arg(instruction, relative_base, 1, registers, ip)
+
+    arg1 = registers[ax] if ax_type == 'position' else ax
+    arg2 = registers[bx] if bx_type == 'position' else bx
+
+    return arg2 if arg1 != 0 else ip + 3
+
+
+def operation_jump_if_false(ip, instruction, relative_base, registers):
+    ax_type, ax = resolve_arg(instruction, relative_base, 0, registers, ip)
+    bx_type, bx = resolve_arg(instruction, relative_base, 1, registers, ip)
+
+    arg1 = registers[ax] if ax_type == 'position' else ax
+    arg2 = registers[bx] if bx_type == 'position' else bx
+
+    return arg2 if arg1 == 0 else ip + 3
+
+
+def operation_less_than(ip, instruction, relative_base, registers):
+    ax_type, ax = resolve_arg(instruction, relative_base, 0, registers, ip)
+    bx_type, bx = resolve_arg(instruction, relative_base, 1, registers, ip)
+    cx_type, cx = resolve_arg(instruction, relative_base, 2, registers, ip)
+
+    arg1 = registers[ax] if ax_type == 'position' else ax
+    arg2 = registers[bx] if bx_type == 'position' else bx
+
+    registers[cx] = 1 if arg1 < arg2 else 0
+
+    return ip + 4
+
+
+def operation_equals(ip, instruction, relative_base, registers):
+    ax_type, ax = resolve_arg(instruction, relative_base, 0, registers, ip)
+    bx_type, bx = resolve_arg(instruction, relative_base, 1, registers, ip)
+    cx_type, cx = resolve_arg(instruction, relative_base, 2, registers, ip)
+
+    arg1 = registers[ax] if ax_type == 'position' else ax
+    arg2 = registers[bx] if bx_type == 'position' else bx
+
+    registers[cx] = 1 if arg1 == arg2 else 0
+
+    return ip + 4
+
+
+def operation_set_relative_base(ip, instruction, relative_base, registers):
+    ax_type, ax = resolve_arg(instruction, relative_base, 0, registers, ip)
+
+    arg1 = registers[ax] if ax_type == 'position' else ax
+
+    return (ip + 2, relative_base + arg1)
+
+
+def run_boost_program(registers, inputs=None):
+    outputs = []
+
+    ip = 0
+    current_input_index = 0
+    relative_base = 0
+
+    operations = {
+        1: operation_add,
+        2: operation_mul,
+        3: operation_get_input,
+        4: operation_set_output,
+        5: operation_jump_if_true,
+        6: operation_jump_if_false,
+        7: operation_less_than,
+        8: operation_equals,
+        9: operation_set_relative_base
+    }
+
+    while True:
+        instruction = '0' * 4 + str(registers[ip])
+
+        opcode = int(instruction[-2:])
+
+        if opcode == 99:
+            break
+
+        if opcode == 3:
+            ip = operations[opcode](
+                ip, instruction, relative_base, registers,
+                inputs[current_input_index])
+
+            current_input_index += 1
+
+        elif opcode == 4:
+            ip, output = operations[opcode](
+                ip, instruction, relative_base, registers)
+
+            outputs.append(output)
+
+        elif opcode == 9:
+            ip, relative_base = operations[opcode](
+                ip, instruction, relative_base, registers)
+
+        else:
+            ip = operations[opcode](
+                ip, instruction, relative_base, registers)
+
+    return outputs
+
+
+def parse_input(data):
+    data = data.strip()
+    return list(map(int, data.split(',')))
+
+
+class TestClass:
+    def test_run_boost_program_0(self):
+        data = '109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99'
+        registers = parse_input(data)
+        outputs = run_boost_program(registers)
+
+        answer = [
+            109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006,
+            101, 0, 99
+        ]
+
+        assert outputs == answer
+
+    def test_run_boost_program_1(self):
+        data = '1102,34915192,34915192,7,4,7,99,0'
+        registers = parse_input(data)
+        outputs = run_boost_program(registers)
+
+        assert len(str(outputs[0])) == 16
+
+    def test_run_boost_program_2(self):
+        data = '104,1125899906842624,99'
+        registers = parse_input(data)
+        outputs = run_boost_program(registers)
+
+        assert outputs[0] == 1125899906842624
+
+
+def main():
+    data = sys.stdin.readline()
+
+    registers = parse_input(data)
+    distress_signal = run_boost_program(registers, [2])[-1]
+
+    print(distress_signal)
+
+
+if __name__ == '__main__':
+    main()
