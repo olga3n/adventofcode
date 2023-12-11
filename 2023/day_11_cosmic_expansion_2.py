b0c8b4c05a6d962485e49c9ba7034ddffdc2e25f
olga3n <olga3n@gmail.com>
2023-12-11 13:03:57 +0300

    11

---
+++ 2023/day_11_cosmic_expansion_2.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import sys
+from typing import List, Set
+
+
+def expanding_rows(data: List[str]) -> Set[int]:
+    rows = set()
+    for i, line in enumerate(data):
+        if all(ch == '.' for ch in line):
+            rows.add(i)
+    return rows
+
+
+def expanding_cols(data: List[str]) -> Set[int]:
+    cols = set()
+    for i in range(len(data[0])):
+        if all(data[j][i] == '.' for j in range(len(data))):
+            cols.add(i)
+    return cols
+
+
+def sum_lengths(data: List[str], extra_lines=2) -> int:
+    rows = expanding_rows(data)
+    cols = expanding_cols(data)
+
+    galaxies = []
+
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j] != '#':
+                continue
+            galaxies.append((i, j))
+
+    result = 0
+
+    for g1, (x1, y1) in enumerate(galaxies):
+        for g2, (x2, y2) in enumerate(galaxies):
+            if g2 >= g1:
+                continue
+
+            dx = max(x1, x2) - min(x1, x2)
+            dy = max(y1, y2) - min(y1, y2)
+
+            for i in range(min(x1, x2), max(x1, x2)):
+                if i in rows:
+                    dx += extra_lines - 1
+
+            for j in range(min(y1, y2), max(y1, y2)):
+                if j in cols:
+                    dy += extra_lines - 1
+
+            result += dx + dy
+
+    return result
+
+
+def test_sum_lengths():
+    data = [
+        '...#......',
+        '.......#..',
+        '#.........',
+        '..........',
+        '......#...',
+        '.#........',
+        '.........#',
+        '..........',
+        '.......#..',
+        '#...#.....',
+    ]
+    assert sum_lengths(data, extra_lines=10) == 1030
+    assert sum_lengths(data, extra_lines=100) == 8410
+
+
+def main():
+    data = (line.rstrip() for line in sys.stdin)
+    data = [line for line in data if len(line)]
+    result = sum_lengths(data, extra_lines=1000000)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
